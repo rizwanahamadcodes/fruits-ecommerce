f133abec4a5dab49496967507d629995f1519c17
rizwan <rizwanahamadcodes@gmail.com>
2023-11-24 15:04:57 +0545

    added date-fns to calculate timeAgo

--- src/pages/Products/Product/Product.tsx
+++ src/pages/Products/Product/Product.tsx
@@ -85,44 +85,6 @@ const Review = (props: ReviewProps) => {
         }).format(date);
     };
 
-    const getTimeAgo = (date: Date) => {
-        const now = new Date();
-
-        const diffTime = now.getTime() - date.getTime();
-        const diffDay = now.getUTCDay() - date.getUTCDay();
-        const diffMonth = now.getUTCMonth() - date.getUTCMonth();
-        const diffYear = now.getUTCFullYear() - date.getUTCFullYear();
-
-        console.log(date.getUTCMonth());
-
-        const timeInMilliseconds = {
-            month: 1000 * 60 * 60 * 24 * 7 * 4,
-            week: 1000 * 60 * 60 * 24 * 7,
-            day: 1000 * 60 * 60 * 24,
-            hour: 1000 * 60 * 60,
-            minute: 1000 * 60,
-            second: 1000,
-        };
-
-        if (diffTime < timeInMilliseconds.month) {
-            for (const unit in timeInMilliseconds) {
-                const particularTimeInMillisecond =
-                    timeInMilliseconds[unit as keyof typeof timeInMilliseconds];
-
-                if (diffTime > particularTimeInMillisecond) {
-                    return {
-                        unit: unit,
-                        value: Math.floor(
-                            diffTime / particularTimeInMillisecond
-                        ),
-                    };
-                }
-            }
-        }
-    };
-
-    console.log(getTimeAgo(new Date("2023/8/31/14:27:00")));
-
     return (
         <div className="flex gap-1">
             <img
