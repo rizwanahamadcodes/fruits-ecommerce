08d9758593da1975e1357c247c165f83e07f358e
rizwan <rizwanahamadcodes@gmail.com>
2024-01-02 16:03:30 +0545

    changed the like and dislike functions such that they counter each other

--- src/store/slices/reviewsSlice.ts
+++ src/store/slices/reviewsSlice.ts
@@ -14,38 +14,50 @@ const reviewsSlice = createSlice({
     } as ReviewsState,
     reducers: {
         likeReview: (state, action: PayloadAction<Review["id"]>) => {
-            state.reviews = state.reviews.map((review) =>
-                review.id === action.payload
-                    ? review.likedAsGuest
-                        ? {
-                              ...review,
-                              likeCount: review.likeCount - 1,
-                              likedAsGuest: false,
-                          }
-                        : {
-                              ...review,
-                              likeCount: review.likeCount + 1,
-                              likedAsGuest: true,
-                          }
-                    : review
-            );
+            state.reviews = state.reviews.map((review) => {
+                if (review.id != action.payload) return review;
+
+                if (!review.likedAsGuest) {
+                    return {
+                        ...review,
+                        likeCount: review.likeCount + 1,
+                        likedAsGuest: true,
+                        dislikedAsGuest: false,
+                        dislikeCount: review.dislikedAsGuest
+                            ? review.dislikeCount - 1
+                            : review.dislikeCount,
+                    };
+                } else {
+                    return {
+                        ...review,
+                        likeCount: review.likeCount - 1,
+                        likedAsGuest: false,
+                    };
+                }
+            });
         },
         dislikeReview: (state, action: PayloadAction<Review["id"]>) => {
-            state.reviews = state.reviews.map((review) =>
-                review.id === action.payload
-                    ? review.dislikedAsGuest
-                        ? {
-                              ...review,
-                              dislikeCount: review.dislikeCount - 1,
-                              dislikedAsGuest: false,
-                          }
-                        : {
-                              ...review,
-                              dislikeCount: review.dislikeCount + 1,
-                              dislikedAsGuest: true,
-                          }
-                    : review
-            );
+            state.reviews = state.reviews.map((review) => {
+                if (review.id != action.payload) return review;
+
+                if (!review.dislikedAsGuest) {
+                    return {
+                        ...review,
+                        dislikeCount: review.dislikeCount + 1,
+                        dislikedAsGuest: true,
+                        likedAsGuest: false,
+                        likeCount: review.likedAsGuest
+                            ? review.likeCount - 1
+                            : review.likeCount,
+                    };
+                } else {
+                    return {
+                        ...review,
+                        dislikeCount: review.dislikeCount - 1,
+                        dislikedAsGuest: false,
+                    };
+                }
+            });
         },
     },
 });
