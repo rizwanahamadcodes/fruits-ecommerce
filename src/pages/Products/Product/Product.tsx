45e46b57105b6d458f21989b34ca2cc7c5e9ebfd
rizwan <rizwanahamadcodes@gmail.com>
2024-01-05 23:41:25 +0545

    Breadcrumb component completed with dynamic product names

--- src/pages/Products/Product/Product.tsx
+++ src/pages/Products/Product/Product.tsx
@@ -1,14 +1,37 @@
 import { useLocation, useParams } from "react-router-dom";
 import { paramConstants, routes } from "../../../routes/pathConstants";
 
+import { useSelector } from "react-redux";
+import Breadcrumb from "../../../components/Breadcrumb";
+import { RootState } from "../../../store/rootReducer";
+import { selectAllProducts } from "../../../store/slices/productsSlice";
 import ProductViewSection from "./ProductViewSection";
 import ReviewsSection from "./ReviewsSection";
-import Breadcrumb from "../../../components/Breadcrumb";
 
 const Product = () => {
     const location = useLocation();
     const param = useParams()[paramConstants.PRODUCTID];
     const productId = param ? parseInt(param) : null;
+    const products = useSelector((state: RootState) =>
+        selectAllProducts(state)
+    );
+
+    const getProductNameById = (id: string) => {
+        const productId = parseInt(id);
+
+        if (productId) {
+            const foundProduct = products.find(
+                (product) => product.id === productId
+            );
+            if (foundProduct) {
+                return foundProduct.name;
+            } else {
+                return "Product Details";
+            }
+        } else {
+            return "Product Details";
+        }
+    };
 
     return (
         <>
@@ -19,9 +42,10 @@ const Product = () => {
                         routes={routes}
                         pathname={location.pathname}
                         dynamicBreadcrumbLabelGenerators={{
-                            productId: (id: string) => "I was dynamic " + id,
+                            productId: (id: string) => getProductNameById(id),
                         }}
                     />
+
                     <ProductViewSection productId={productId} />
                     <ReviewsSection productId={productId} />
                 </>
