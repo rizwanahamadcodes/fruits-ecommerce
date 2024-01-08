b844b4c06d27be39f222ecfa268e893d27e9d4aa
rizwan <rizwanahamadcodes@gmail.com>
2024-01-08 15:06:22 +0545

    completed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,171 @@
+import { IconType } from "react-icons";
+import { BsFacebook, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+import { Link } from "react-router-dom";
+import BrandLogo from "./BrandLogo";
 import Container from "./Container";
 import Section from "./Section";
 import SignUpForNewsLetter from "./SignUpForNewsLetter";
+import pathConstants from "../routes/pathConstants";
 
+const footerLinks = [
+    {
+        label: "Home",
+        href: "/",
+        icon: "",
+    },
+    {
+        label: "Products",
+        href: "/products",
+        icon: "",
+    },
+
+    {
+        label: "FAQs",
+        href: "/faq",
+        icon: "",
+    },
+    {
+        label: "Contact",
+        href: "/contact",
+        icon: "",
+    },
+];
+
+const socialLinks = [
+    {
+        label: "Facebook",
+        href: "https://www.facebook.com",
+        icon: BsFacebook,
+    },
+    {
+        label: "Instagram",
+        href: "https://www.instagram.com",
+        icon: BsInstagram,
+    },
+    {
+        label: "LinkedIn",
+        href: "https://www.linkedin.com/",
+        icon: BsLinkedin,
+    },
+];
+const FooterContactLinks = [
+    {
+        label: "avocadoes",
+        href: "mailto:rizwanahamadcodes@gmail.com",
+        icon: SiGmail,
+    },
+];
 const Footer = () => {
     return (
-        <footer className="bg-gray-200 mt-3 lg:mt-1">
+        <footer className="bg-gray-50 mt-5 lg:mt-4">
             <Container className="relative">
                 <div className="absolute w-full -translate-y-1/2">
                     <SignUpForNewsLetter />
                 </div>
             </Container>
             <Section className="">
-                <Container className="mt-3 lg:mt-1.5"></Container>
+                <Container className="mt-4 gap-2 lg:mt-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <FooterColumn>
+                        <FooterColumnTitle className="flex items-center gap-0.75">
+                            <BrandLogo />
+                            <Link
+                                to={pathConstants.HOME.path}
+                                className="text-1">
+                                Avocadoes
+                            </Link>
+                        </FooterColumnTitle>
+
+                        <FooterColumnBody>
+                            <p>
+                                Welcome to Avocadoes! Your one-stop shop for
+                                fresh fruits and vegetables. We're all about
+                                delivering quality produce to your doorstep.
+                                Taste the freshness, shop with us today
+                            </p>
+                        </FooterColumnBody>
+                    </FooterColumn>
+                    <FooterColumn>
+                        <FooterColumnTitle>Browse</FooterColumnTitle>
+                        <FooterColumnBody>
+                            <FooterColumnLinks links={footerLinks} />
+                        </FooterColumnBody>
+                    </FooterColumn>
+                    <FooterColumn>
+                        <FooterColumnTitle>Socials</FooterColumnTitle>
+                        <FooterColumnBody>
+                            <FooterColumnLinks links={socialLinks} />
+                        </FooterColumnBody>
+                    </FooterColumn>
+
+                    <FooterColumn>
+                        <FooterColumnTitle>Contact</FooterColumnTitle>
+                        <FooterColumnBody>
+                            <FooterColumnLinks links={FooterContactLinks} />
+                        </FooterColumnBody>
+                    </FooterColumn>
+                </Container>
             </Section>
         </footer>
+    );
+};
+
+type FooterColumnProps = {
+    children: React.ReactNode;
+};
+
+const FooterColumn = (props: FooterColumnProps) => {
+    const { children } = props;
+    return <div className="flex flex-col gap-1">{children}</div>;
+};
+
+type FooterColumnTitleProps = {
+    children: React.ReactNode;
+    className?: string;
+};
+
+const FooterColumnTitle = (props: FooterColumnTitleProps) => {
+    const { className, children } = props;
+    return <h4 className={className}>{children}</h4>;
+};
+
+type FooterColumnBodyProps = {
+    children: React.ReactNode;
+};
+
+const FooterColumnBody = (props: FooterColumnBodyProps) => {
+    const { children } = props;
+    return <div>{children}</div>;
+};
+
+type FooterLinkType = {
+    label: string;
+    href: string;
+    icon: string | IconType;
+};
+type FooterColumnLinksProps = {
+    links: FooterLinkType[];
+};
+const FooterColumnLinks = (props: FooterColumnLinksProps) => {
+    const { links } = props;
+
+    return (
+        <ul className="flex flex-col gap-0.25">
+            {links.map((link) => {
+                const { icon: Icon } = link;
+
+                return (
+                    <li key={link.href} className="flex gap-0.5 items-center">
+                        {typeof Icon === "string" ? null : (
+                            <Icon className="text-xl" />
+                        )}
+                        <Link className="!font-medium" to={link.href}>
+                            {link.label}
+                        </Link>
+                    </li>
+                );
+            })}
+        </ul>
     );
 };
 
---
+++ src/components/FooterContactLinks.tsx
@@ -0,0 +1,7 @@
+export const FooterContactLinks = [
+    {
+        label: "rizwanahamadcodes@gmail",
+        href: "mailto:rizwanahamadcodes@gmail.com",
+        icon: SiGmail,
+    },
+];
