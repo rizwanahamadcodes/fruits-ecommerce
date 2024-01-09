2ff206c2bdd16eb3c832abbc1b5231e1a317d2bc
rizwan <rizwanahamadcodes@gmail.com>
2024-01-09 13:56:27 +0545

    completed the contact us section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -57,7 +57,7 @@ const FooterContactLinks = [
 ];
 const Footer = () => {
     return (
-        <footer className="bg-gray-50 border border-t-gray-200">
+        <footer className="bg-gray-50 border-t-[1px] border-t-gray-200">
             <Section>
                 <Container className="gap-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     <FooterColumn>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,42 @@
+import Container from "../components/Container";
+import Section, { SectionSubtitle, SectionTitle } from "../components/Section";
+import { Input } from "./Checkout";
+
 const Contact = () => {
-    return <div>Contact</div>;
+    return (
+        <Section className="bg-white">
+            <div className="h-navHeight"></div>
+            <Container className="flex">
+                <div className="grow">
+                    <SectionTitle>Contact Us</SectionTitle>
+                    <SectionSubtitle>
+                        For any site related queries or feedback, contact us
+                    </SectionSubtitle>
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                        }}
+                        className="flex flex-col gap-1">
+                        <Input
+                            rounded="rounded-full"
+                            placeholder="Name"
+                            type="text"
+                        />
+                        <Input
+                            rounded="rounded-full"
+                            placeholder="Name"
+                            type="email"
+                        />
+                        <textarea
+                            className="border-gray-200 grow border hover:border-primary text-1 max-w-lg px-1 focus:outline-none  focus:shadow-primary-border focus:border-primary transition rounded-1.5 h-6 pt-1 resize-none"
+                            placeholder="Leave a message"
+                        />
+                    </form>
+                </div>
+                <img src="/images/helpdesk.svg" className="w-1/3"></img>
+            </Container>
+        </Section>
+    );
 };
 
 export default Contact;
