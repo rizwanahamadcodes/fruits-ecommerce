daa88816a2e571cfd0836d6cf6bbdff6876c227d
Rizwan <135693068+rizwanahamadcodes@users.noreply.github.com>
2025-02-17 12:20:28 +0545

    Update newPathConstants.js

--- newPathConstants.js
+++ newPathConstants.js
@@ -70,3 +70,73 @@ export const PATHCONSTANTS = {
         },
     },
 };
+
+export const PATHCONSTANTS = {
+    PRODUCTS: {
+        pathSegment: "products",
+        param: "productId",
+        ADD: {
+            pathSegment: "new",
+        },
+        EDIT: {
+            pathSegment: "edit",
+        },
+        DELETE: {
+            pathSegment: "delete",
+        },
+        DETAIL: {
+            pathSegment: "detail",
+        },
+    },
+    USERS: {
+        pathSegment: "users",
+        param: "userId",
+        ADD: {
+            pathSegment: "new",
+        },
+        EDIT: {
+            pathSegment: "edit",
+        },
+        DELETE: {
+            pathSegment: "delete",
+        },
+        DETAIL: {
+            pathSegment: "detail",
+        },
+    },
+};
+
+// Now assign the full paths after the basic structure is initialized
+PATHCONSTANTS.PRODUCTS.path = `/${PATHCONSTANTS.PRODUCTS.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.ADD.path = `/${PATHCONSTANTS.PRODUCTS.pathSegment}/${PATHCONSTANTS.PRODUCTS.ADD.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.EDIT.path = `:${PATHCONSTANTS.PRODUCTS.param}/${PATHCONSTANTS.PRODUCTS.EDIT.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.DELETE.path = `:${PATHCONSTANTS.PRODUCTS.param}/${PATHCONSTANTS.PRODUCTS.DELETE.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.DETAIL.path = `:${PATHCONSTANTS.PRODUCTS.param}/${PATHCONSTANTS.PRODUCTS.DETAIL.pathSegment}`;
+
+PATHCONSTANTS.USERS.path = `/${PATHCONSTANTS.USERS.pathSegment}`;
+PATHCONSTANTS.USERS.ADD.path = `/${PATHCONSTANTS.USERS.pathSegment}/${PATHCONSTANTS.USERS.ADD.pathSegment}`;
+PATHCONSTANTS.USERS.EDIT.path = `:${PATHCONSTANTS.USERS.param}/${PATHCONSTANTS.USERS.EDIT.pathSegment}`;
+PATHCONSTANTS.USERS.DELETE.path = `:${PATHCONSTANTS.USERS.param}/${PATHCONSTANTS.USERS.DELETE.pathSegment}`;
+PATHCONSTANTS.USERS.DETAIL.path = `:${PATHCONSTANTS.USERS.param}/${PATHCONSTANTS.USERS.DETAIL.pathSegment}`;
+
+// Now, define the buildHref methods
+PATHCONSTANTS.PRODUCTS.buildHref = () => `/${PATHCONSTANTS.PRODUCTS.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.ADD.buildHref = () =>
+    `/${PATHCONSTANTS.PRODUCTS.pathSegment}/${PATHCONSTANTS.PRODUCTS.ADD.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.EDIT.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.PRODUCTS.EDIT.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.DELETE.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.PRODUCTS.DELETE.pathSegment}`;
+PATHCONSTANTS.PRODUCTS.DETAIL.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.PRODUCTS.DETAIL.pathSegment}`;
+
+PATHCONSTANTS.USERS.buildHref = () => `/${PATHCONSTANTS.USERS.pathSegment}`;
+PATHCONSTANTS.USERS.ADD.buildHref = () =>
+    `/${PATHCONSTANTS.USERS.pathSegment}/${PATHCONSTANTS.USERS.ADD.pathSegment}`;
+PATHCONSTANTS.USERS.EDIT.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.USERS.EDIT.pathSegment}`;
+PATHCONSTANTS.USERS.DELETE.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.USERS.DELETE.pathSegment}`;
+PATHCONSTANTS.USERS.DETAIL.buildHref = (param) =>
+    `/${param}/${PATHCONSTANTS.USERS.DETAIL.pathSegment}`;
+
